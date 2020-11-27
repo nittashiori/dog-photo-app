1590d0e1486b4439c1d5023a5ea7872f2f48dc15
nittashiori <nitta@impv.co.jp>
2020-11-27 18:51:28 +0900

    storeに登録したデータをページに表示

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -2,11 +2,17 @@
   <div class="container">
     <div>コンテンツが入るよ</div>
     <div>{{ dogs }}</div>
+    <div class="columns is-multiline">
+      <div v-for="(item, i) in doglist" :key="i" class="column is-2">
+        <a class="button">{{ i }}</a>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import axios from 'axios'
+import { mapGetters } from 'vuex'
 
 export default {
   async asyncData({ store }) {
@@ -16,6 +22,9 @@ export default {
     await store.dispatch('register/setDogs', dogs)
 
     return { dogs }
+  },
+  computed: {
+    ...mapGetters('register', ['doglist']),
   },
 }
 </script>
