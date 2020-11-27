3a94978b223d6023ccbca63e87f6d41498d6b9b3
nittashiori <nitta@impv.co.jp>
2020-11-27 18:51:09 +0900

    APIから取得したデータをstoreに登録

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,3 +1,21 @@
 <template>
-  <div class="container">コンテンツが入るよ</div>
+  <div class="container">
+    <div>コンテンツが入るよ</div>
+    <div>{{ dogs }}</div>
+  </div>
 </template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  async asyncData({ store }) {
+    const response = await axios.get('https://dog.ceo/api/breeds/list/all')
+    const dogs = response.data.message
+
+    await store.dispatch('register/setDogs', dogs)
+
+    return { dogs }
+  },
+}
+</script>
