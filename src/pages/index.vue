61cfba69b85f8c8bab1b1e967a4b7fd658190034
nittashiori <nitta@impv.co.jp>
2020-12-01 17:28:39 +0900

    名前変更、下層ページ遷移追加

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,9 +1,11 @@
 <template>
   <div class="container">
-    <div>コンテンツが入るよ</div>
     <div class="columns is-multiline">
-      <div v-for="(item, i) in doglist" :key="i" class="column is-2">
-        <a class="button">{{ i }}</a>
+      <div v-for="(item, i) in dogNameList" :key="i" class="column is-2">
+        <!-- <a class="button">{{ i }}</a> -->
+        <nuxt-link :to="{ path: 'dogs/' + i }" class="button">
+          {{ i }}
+        </nuxt-link>
       </div>
     </div>
   </div>
@@ -16,14 +18,14 @@ import { fetchDogName } from '~/api/fetchDogName'
 export default {
   async asyncData({ store }) {
     const response = await fetchDogName
-    const dogsname = response
+    const dogname = response
 
-    await store.dispatch('register/setDogs', dogsname)
+    await store.dispatch('register/setDogs', dogname)
 
-    return { dogsname }
+    return { dogname }
   },
   computed: {
-    ...mapGetters('register', ['doglist']),
+    ...mapGetters('register', ['dogNameList']),
   },
 }
 </script>
