1743892c554f641e405ae10483e481acdbfe60d4
nittashiori <nitta@impv.co.jp>
2020-12-03 18:38:56 +0900

    title、description、ogp、favicon追加

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,8 +10,62 @@ export default {
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
+      {
+        hid: 'og:site_name',
+        property: 'og:site_name',
+        content: 'dog-photo-app',
+      },
+      { hid: 'og:type', property: 'og:type', content: 'website' },
+      {
+        hid: 'og:url',
+        property: 'og:url',
+        content: 'https://dog-photo-app.netlify.app/',
+      },
+      {
+        hid: 'og:title',
+        property: 'og:title',
+        content: 'dog photo app',
+      },
+      {
+        hid: 'og:description',
+        property: 'og:description',
+        content:
+          'DogAPIを使用して犬種ごとに画像一覧を表示するサイトです。',
+      },
+      {
+        hid: 'og:image',
+        property: 'og:image',
+        content:
+          'https://dog-photo-app.netlify.app/images/ogp/common.png',
+      },
+      {
+        hid: 'twitter:card',
+        property: 'twitter:card',
+        content: 'summary_large_image',
+      },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    link: [
+      {
+        rel: 'icon',
+        type: 'image/x-icon',
+        href: '/images/favicon/favicon.ico',
+      },
+      {
+        rel: 'icon',
+        type: 'image/svg+xml',
+        href: '/images/favicon/favicon.svg',
+      },
+      {
+        rel: 'icon alternate',
+        type: 'image/png',
+        href: '/images/favicon/favicon.png',
+      },
+      {
+        rel: 'apple-touch-icon',
+        size: '180x180',
+        href: '/images/apple-touch-icon/common.png',
+      },
+    ],
   },
 
   // Global CSS (https://go.nuxtjs.dev/config-css)
