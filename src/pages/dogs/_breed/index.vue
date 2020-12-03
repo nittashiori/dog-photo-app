962b99c8203fa6dbba6ae6c16be1bbee6ab03c3a
nittashiori <nitta@impv.co.jp>
2020-12-03 18:23:27 +0900

    1ページ目の最初の3枚にだけnewラベルがつくようにした

--- src/pages/dogs/_breed/index.vue
+++ src/pages/dogs/_breed/index.vue
@@ -4,7 +4,7 @@
     <div class="columns is-multiline">
       <div v-for="(item, i) in dogImageList" :key="i" class="column is-3">
         <img :src="item.url" />
-        <span v-if="i < 3" class="tag is-danger">NEW</span>
+        <span v-if="current === 1 && i < 3" class="tag is-danger">NEW</span>
         <a class="button is-warning is-small" @click="item.like += 1">
           <span>いいね!{{ item.like }}件</span>
         </a>
